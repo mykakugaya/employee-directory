270a959ea0917cf0ad662355a34c7c3556f38870
Myka Kugaya <mykakug129@gmail.com>
2020-06-21 20:35:41 -0700

    Updates

---
+++ precache-manifest.1bcee756ac6a9b73270ba44c657f0225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2407466d959f49da74e656ca1b9b18ae",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "0fe71aecc3763018fe15",
+    "url": "/employee-directory/static/css/main.2eb45999.chunk.css"
+  },
+  {
+    "revision": "900582180b1bca4d555b",
+    "url": "/employee-directory/static/js/2.14cf8614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.14cf8614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe71aecc3763018fe15",
+    "url": "/employee-directory/static/js/main.02c9f99b.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/employee-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
